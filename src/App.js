59d476265ea2b8567cce150a5d4ebcffe69e742c
edgardsoza <120115282+edgardsoza@users.noreply.github.com>
2023-03-28 13:46:30 -0600

    Fix reviewer recommendations

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
-import './App.css';
 import Calculator from './components/calculator';
 
 function App() {
   return (
-    <div className="App">
-      <body className="App-header">
-        <p>
-          <Calculator/>
-        </p>
-      </body>
-    </div>
+    <Calculator/>
   );
 }
 
