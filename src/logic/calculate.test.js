d58013928ad5eaacc815590781b45f4f8d14ff02
edgardsoza <120115282+edgardsoza@users.noreply.github.com>
2023-04-05 13:21:23 -0600

    create tests for operate and calculate

--- src/logic/calculate.test.js
+++ src/logic/calculate.test.js
@@ -1,3 +1,94 @@
-it ('test test', () => {
-    expect(1).toBe(1)
+import calculate from './calculate';
+import isNumber from './calculate';
+
+describe ('testing calculate functions', () => {
+it ('testing calculate', () => {
+    const object = {
+      total: 0,
+      next: 7,
+      operation: '+'
+    }
+    const newObject = calculate(object, 'AC');
+    expect(newObject.total).toBe(0)
+});
+
+it ('testing calculate sum', () => {
+    const object = {
+      total: 3,
+      next: 7,
+      operation: '+'
+    }
+    const newObject = calculate(object, '=');
+    expect(newObject.total).toBe('10')
+});
+
+it ('testing calculate substraction', () => {
+    const object = {
+      total: 9,
+      next: 17,
+      operation: '-'
+    }
+    const newObject = calculate(object, '');
+    expect(newObject.total).toBe('-8')
+});
+
+it ('testing calculate +/-', () => {
+    const object = {
+      total: 9,
+      next: null,
+      operation: '+/-'
+    }
+    const newObject = calculate(object, '+/-');
+    expect(newObject.total).toBe('-9')
+});
+
+it ('testing calculate divide', () => {
+    const object = {
+      total: 72,
+      next: 9,
+      operation: '÷'
+    }
+    const newObject = calculate(object, '');
+    expect(newObject.total).toBe('8')
+});
+
+it ('testing calculate multiply', () => {
+    const object = {
+      total: 7,
+      next: 9,
+      operation: 'x'
+    }
+    const newObject = calculate(object, '');
+    expect(newObject.total).toBe('63')
+});
+
+it ('testing calculate button 0', () => {
+    const object = {
+      total: 7,
+      next: 0,
+      operation: ''
+    }
+    const newObject = calculate(object, '0');
+    expect(newObject.total).toBe(null)
+});
+
+it ('testing calculate button 0', () => {
+    const object = {
+      total: 0,
+      next: 10,
+      operation: '+'
+    }
+    const newObject = calculate(object, '+');
+    expect(newObject).toStrictEqual({"operation": "+", "total": 0});
+});
+
+it ('testing calculate button .', () => {
+    const object = {
+      total: 5,
+      next: '.',
+      operation: ''
+    }
+    const newObject = calculate(object, '');
+    expect(newObject).toStrictEqual({'operation': '', "total": '.', 'next': null});
+});
 });
--- src/logic/operate.test.js
+++ src/logic/operate.test.js
@@ -1,20 +1,31 @@
 import operate from './operate';
 
-
 describe('test operations in calculator', () => {
-test ('sum test', () => {
+it ('sum test', () => {
     expect(operate(1,2,'+')).toBe('3')
 });
 
-test ('substraction test', () => {
+it ('substraction test', () => {
     expect(operate(5,2,'-')).toBe('3')
 });
 
-test ('divide test', () => {
+it ('divide test', () => {
     expect(operate(8,2,'÷')).toBe('4')
 });
 
-test ('divide test', () => {
+it ('divide test zero', () => {
+    expect(operate(8,0,'÷')).toBe('Can\'t divide by 0.')
+});
+
+it ('multiply test', () => {
     expect(operate(8,2,'x')).toBe('16')
+});
+
+it ('percentage test', () => {
+    expect(operate(5,7,'%')).toBe('5')
+});
+
+it ('percentage test zero', () => {
+    expect(operate(5,0,'%')).toBe('Can\'t find modulo as can\'t divide by 0.')
 });
 });
